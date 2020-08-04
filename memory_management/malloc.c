e9b7b9c3a3391868cdc4165d954de3b13d1be4fc
Arsen Muradyan <arsenmuradyan604@gmail.com>
2020-08-04 23:25:24 +0400

    Malloc

---
+++ memory_management/malloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int *ptr;
+  ptr = malloc(10 * sizeof(int));
+  *(ptr) = 30;
+  *(ptr+2) = 50;
+  // Use += statment
+  *(ptr+=3) = 20;
+  *(ptr+6) = 60;
+  for (int i = 0; i < 10; i++) {
+    printf("Value: %d, INDEX: %d\n", *(ptr+i), i);
+  }
+  free(ptr);
+  return 0;
+}
