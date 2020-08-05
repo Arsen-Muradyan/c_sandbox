88c3bddf47be8660788c0726d6b3418f84eedbcb
Arsen Muradyan <arsenmuradyan604@gmail.com>
2020-08-05 20:45:35 +0400

    Calloc

---
+++ memory_management/calloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct {
+  int age;
+  char* info;
+} record;
+
+int main() {
+  int num_recs = 2;
+  record *recs;
+  char str[] = "Simple info!";
+  recs = calloc(num_recs, sizeof(record));
+  if (recs != NULL) {
+    for (int i = 0; i < num_recs; i++) {
+      (recs+i)->age = i+20;
+      (recs+i)->info = malloc(sizeof(str));
+      strcpy((recs+i)->info, str);
+    }
+  }
+  printf("%s\n", (recs+1)->info);
+  return 0;
+}
