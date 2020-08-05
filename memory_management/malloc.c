954fd3f5c1a4c6bbdbd1a6b19556a40de64f235a
Arsen Muradyan <arsenmuradyan604@gmail.com>
2020-08-05 21:18:27 +0400

    Memory Management

--- memory_management/malloc.c
+++ memory_management/malloc.c
@@ -3,16 +3,16 @@
 
 int main() {
   int *ptr;
-  ptr = malloc(10 * sizeof(int));
+  ptr = malloc(10 * sizeof(*ptr));
   *(ptr) = 30;
   *(ptr+2) = 50;
-  // Use += statment
-  *(ptr+=3) = 20;
-  *(ptr+6) = 60;
-  // Use Loop
-  for (int i = 0; i < 10; i++) {
-    printf("Value: %d, INDEX: %d\n", *(ptr+i), i);
+  // Use ++ statment
+  *(ptr++) = 20;
+  *(ptr+2) = 60;
+  if (ptr != NULL) {
+    printf("Value: %d, INDEX: %d\n", *(ptr+1), 1);
   }
-  free(ptr);
+  // When using ++ statment free raising error
+  // free(ptr);
   return 0;
 }
